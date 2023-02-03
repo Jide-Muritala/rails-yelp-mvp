82dd3f0c384e4f121e028376465d53e29c8eb382
Babajide Muritala <jide142000@yahoo.com>
2023-02-03 22:23:46 +0100

    create review model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_03_161521) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_03_211939) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -20,4 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_03_161521) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
