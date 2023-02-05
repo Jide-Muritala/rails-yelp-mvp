f8c4089b9bef0def3760b4fa49d36c4b7185b4b0
Babajide Muritala <jide142000@yahoo.com>
2023-02-05 09:12:47 +0100

    create route and controller for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'review/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "restaurants", to: "restaurants#index"
+  get "restaurants", to: "restaurants#index", as: :retaurants
   get "restaurants/new", to: "restaurants#new"
   patch "restaurants", to: "restaurants#create"
-  get "restaurants/:id", to: "restaurants#show"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  resources :restaurants do
+    resources :reviews, only: [:new]
+  end
 end
