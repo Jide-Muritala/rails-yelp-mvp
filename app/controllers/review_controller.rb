be200067f1e47931d721c8e01b653fc7072ea375
Babajide Muritala <jide142000@yahoo.com>
2023-02-05 09:54:01 +0100

    create view for new review

--- app/controllers/review_controller.rb
+++
@@ -1,6 +0,0 @@
-class ReviewController < ApplicationController
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
-end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,16 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant
+
   def new
+    @review = Review.new
+  end
+
+  def create
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
