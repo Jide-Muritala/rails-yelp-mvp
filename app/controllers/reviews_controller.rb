cde453cdcbb9d4d9bfbfb6f63d173559b03ead1e
Babajide Muritala <jide142000@yahoo.com>
2023-02-05 11:28:12 +0100

    create view for new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,11 +6,19 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
   end
 
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "restaurants", to: "restaurants#index", as: :retaurants
+  get "restaurants", to: "restaurants#index", as: :restaurants
   get "restaurants/new", to: "restaurants#new"
   patch "restaurants", to: "restaurants#create"
   get "restaurants/:id", to: "restaurants#show", as: :restaurant
   resources :restaurants do
-    resources :reviews, only: [:new]
+    resources :reviews, only: [:new, :create]
   end
 end
